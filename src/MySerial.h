1e8ba1aa4365f3f48df4fabf749e0334ce211aa5
vudinhso <vudinhso@gmail.com>
2024-06-19 11:23:52 +0700

    Initial commit

---
+++ src/MySerial.h
@@ -0,0 +1,44 @@
+#include <Arduino.h>
+
+unsigned long Serial_time = 0; // time in us
+// ====================================================================================
+void Init_Serial()
+{
+  Serial.begin(115200);
+  while (!Serial)
+    ;
+}
+// ====================================================================================
+void SerialDataPrint()
+{
+  if (micros() - Serial_time >= 10000)
+  {
+    Serial_time = micros();
+    Serial.println(Serial_time / 10000);
+    Serial.print(">MOT1_cmd:");
+    Serial.println(MOT1_cmd);
+  }
+}
+// ====================================================================================
+void SerialDataWrite()
+{
+  static String received_chars;
+  while (Serial.available())
+  {
+    char inChar = (char)Serial.read();
+    received_chars += inChar;
+    if (inChar == '\n')
+    {
+      switch (received_chars[0])
+      {
+      case 'q':
+        received_chars.remove(0, 1);
+        MOT1_cmd = received_chars.toFloat();
+        break;
+      default:
+        break;
+      }
+      received_chars = "";
+    }
+  }
+}
